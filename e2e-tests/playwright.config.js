a8c2f1b9beb611b86d29b2d5394e6d03b62f983e
Amanda <amandauppgard@gmail.com>
2026-03-12 15:43:00 +0200

    increase timeout, remove browsers

--- e2e-tests/playwright.config.js
+++ e2e-tests/playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  timeout: 3000,
+  timeout: 15000,
   fullyParallel: false,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -39,16 +39,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
 
     /* Test against mobile viewports. */
